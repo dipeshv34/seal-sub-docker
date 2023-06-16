424f2348ba742c68231a411bc1b82f961258c834
Dipesh verma Sunkara <dipeshv20@gmail.com>
2023-06-16 19:20:30 +0530

    Added stack logs

--- src/config/logging.php
+++ src/config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'stackdriver'],
             'ignore_exceptions' => false,
         ],
 
@@ -117,6 +117,11 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        'stackdriver' => [
+            'driver' => 'stackdriver',
+            'level' => 'debug',
+        ],
+
     ],
 
 ];
